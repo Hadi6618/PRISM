94ecec048244f667d705c735935e0bd59bc7ac41
Hadi <hade3857@gmail.com>
2026-07-02 07:12:29 +0300

    Temp

--- Others/Helpful Codes for Agents/inspect_nb.py
+++
@@ -1,9 +0,0 @@
-import json, sys
-sys.stdout.reconfigure(encoding="utf-8")
-nb = json.load(open("ShanghaiTech_Ensemble_Fusion.ipynb", encoding="utf-8"))
-# Print ALL cells to see Cell 7
-for i, cell in enumerate(nb["cells"]):
-    src = "".join(cell["source"])
-    print(f"=== Cell {i} (type={cell['cell_type']}) ===")
-    print(src[:600])
-    print()
